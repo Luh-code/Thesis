280ea0b0d5ed4c7f63dae9bd6efa11f5fe55a230
luh <Lasse@hueffler.de>
2022-10-26 12:27:11 +0200

    fixed a potential bug

--- src/vk/vulkan-instance.cpp
+++ src/vk/vulkan-instance.cpp
@@ -6,7 +6,7 @@ namespace Ths::Vk
     {
         const char* validationLayer = "VK_LAYER_KHRONOS_validation";
         if (pLayers->size() > 0) {
-            for (std::vector<const char*>::iterator i = pLayers->begin(); *i != validationLayer; i++)
+            for (std::vector<const char*>::iterator i = pLayers->begin(); strcmp(*i, validationLayer) != 0; i++)
                 if (i == pLayers->end()) {
             pLayers->push_back(validationLayer);
             break;
@@ -14,7 +14,7 @@ namespace Ths::Vk
 
         const char* debugExtension = VK_EXT_DEBUG_UTILS_EXTENSION_NAME;
         if (pExtensions->size() > 0) {
-            for (std::vector<const char*>::iterator i = pExtensions->begin(); *i != debugExtension; i++)
+            for (std::vector<const char*>::iterator i = pExtensions->begin(); strcmp(*i, debugExtension) != 0; i++)
                 if (i == pExtensions->end()) {
             pExtensions->push_back(debugExtension);
             break;
